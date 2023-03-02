df39df4ab11afe38f34dcfcdc3644dad62aa9765
PappNiki20 <papp.nikoletta001@gmail.com>
2023-03-02 13:47:42 +0100

    ismetles nelkul

--- SzamolosFeladatok/src/Ciklusok/Szamok.java
+++ SzamolosFeladatok/src/Ciklusok/Szamok.java
@@ -1,16 +1,34 @@
 package Ciklusok;
 
+import java.util.Scanner;
+
 public class Szamok {
 
     public static void main(String[] args) {
-//        String a = "2";
-//        String b = "3";
-//        String c = "4";
-        for (int i = 2; i < 4; i++) {
-            for (int a = 2; a < 4; a++) {
-                for (int j = 2; j < 4; j++) {
-                    System.out.println(1 + "" + a + "" + j);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ismétléssel, vagy ismétlés nélkül kéri? (igen, nem)");
+        String valasz = sc.nextLine();
+       String eredmeny = valasz;
+        if (eredmeny == "igen") {
+            for (int i = 2; i < 4; i++) {
+                for (int a = 2; a < 4; a++) {
+                    for (int j = 2; j < 4; j++) {
+                        System.out.println(i + "" + a + "" + j);
 
+                    }
+                }
+            }
+
+        } else if(eredmeny == "nem"){
+            for (int i = 2; i < 4; i++) {
+                for (int a = 2; a < 4; a++) {
+                    for (int j = 2; j < 4; j++) {
+                        if(i!=a && i!=j || a!=i && a!=j || j!=i && j !=a){
+                            System.out.println(i + "" + a + "" + j);
+                        }
+                        
+
+                    }
                 }
             }
         }
